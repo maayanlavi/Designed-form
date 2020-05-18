224d0ea3293bc98d31552851b3e97b1f0e2ee0e0
maayanlavi <61950028+maayanlavi@users.noreply.github.com>
2020-05-18 16:31:58 +0300

    Add files via upload

---
+++ ex4/js/main.js
@@ -0,0 +1,38 @@
+var nameCheckFalse = document.getElementById("nameCheckFalse");
+var slider = new Slider('#commandments', {
+	formatter: function(value) {
+		return 'Current value: ' + value;
+	}
+});
+
+    (function() {
+        const form = document.querySelector('#myForm');
+        const checkboxes = form.querySelectorAll('input[type=checkbox]');
+        const checkboxLength = checkboxes.length;
+        const firstCheckbox = checkboxLength > 0 ? checkboxes[0] : null;
+    
+        function init() {
+            if (firstCheckbox) {
+                for (let i = 0; i < checkboxLength; i++) {
+                    checkboxes[i].addEventListener('change', checkValidity);
+                }
+    
+                checkValidity();
+            }
+        }
+    
+        function isChecked() {
+            for (let i = 0; i < checkboxLength; i++) {
+                if (checkboxes[i].checked) return true;
+            }
+    
+            return false;
+        }
+    
+        function checkValidity() {
+            const errorMessage = !isChecked() ? 'At least one checkbox must be selected.' : '';
+            firstCheckbox.setCustomValidity(errorMessage);
+        }
+    
+        init();
+    })();
